0972f4901b3163570c7c90e24678033f70365e8d
Sandeep Saurav <71926868+Sandeep10021@users.noreply.github.com>
2021-01-09 23:19:28 +0530

    Update FaceDetection.py

--- FaceDetection.py
+++ FaceDetection.py
@@ -17,3 +17,18 @@ while True:
         minNeighbors=5,     
         minSize=(20, 20)
     )
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]  
+
+    cv2.imshow('video',img)
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27: # press 'ESC' to quit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
